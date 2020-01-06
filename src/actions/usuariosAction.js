f9c0bb23317e64c6ee06d50a8d330bc1a8a273b7
MistyBlunch <gracenikole@gmail.com>
2020-01-06 15:08:08 -0500

    :skull: Error and Loading states added

--- src/actions/usuariosAction.js
+++ src/actions/usuariosAction.js
@@ -1,12 +1,25 @@
 import axios from "axios";
+import { TRAER_TODOS, CARGANDO, ERROR } from "../types/usuariosTypes";
 
 export const traerTodos = () => async dispatch => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-
   dispatch({
-    type: "traer_usuarios",
-    payload: response.data
+    type: CARGANDO
   });
+
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/users"
+    );
+
+    dispatch({
+      type: TRAER_TODOS,
+      payload: response.data
+    });
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    dispatch({
+      type: ERROR,
+      payload: error.message
+    });
+  }
 };
