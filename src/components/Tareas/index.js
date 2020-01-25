d60f1247904e9431dfb79556fba737f0ea60ae31
MistyBlunch <gracenikole@gmail.com>
2020-01-25 10:54:09 -0500

    :new_moon_with_face: Mapeando las tareas(objetos) del usuario

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Spinner from '../general/Spinner';
+import Fatal from '../general/Fatal';
 
 // Conectar actions al Componente
 import * as tareasActions from '../../actions/tareasActions'
@@ -9,11 +11,50 @@ class Tareas extends Component {
     this.props.traerTodas();
   }
 
+  mostrarContenido = () => {
+    const { tareas, cargando, error } = this.props;
+
+    if (error) {
+      return <Fatal mensaje={error} />
+    }
+
+    if (cargando) {
+      return <Spinner />
+    }
+
+    return Object.keys(tareas).map((user_id) => (
+      <div key={user_id}>
+        <h2>
+          Usuario {user_id}
+        </h2>
+        <div className="contenedor_tareas">
+          {this.ponerTareas(user_id)}
+        </div>
+      </div>
+    ))
+  };
+
+  ponerTareas = (user_id) => {
+    const { tareas } = this.props;
+    const por_usuario = {
+      ...tareas[user_id]
+    };
+
+    return Object.keys(por_usuario).map((tar_id) => (
+      <div key={tar_id}>
+        <input type="checkbox" defaultChecked={por_usuario[tar_id].completed} />
+        {
+          por_usuario[tar_id].title
+        }
+      </div>
+    ));
+  };
+
   render() {
     console.log(this.props)
     return (
       <div>
-        Tareas Saludar
+        {this.mostrarContenido()}
       </div>
     )
   }
