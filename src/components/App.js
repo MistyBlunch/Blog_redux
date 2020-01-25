89a4be0579a0f947a892e0e0787d4a91ffbefe66
MistyBlunch <gracenikole@gmail.com>
2020-01-25 16:13:07 -0500

    :octocat: Formulario para agregar tareas

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Menu from "./Menu";
 import Usuarios from "./usuarios";
 import Publicaciones from "./Publicaciones";
 import Tareas from './Tareas'
+import TareasGuardar from './Tareas/Guardar';
 
 const App = () => (
   <BrowserRouter>
@@ -13,6 +14,7 @@ const App = () => (
       <Route exact path="/" component={Usuarios} />
       <Route exact path="/tareas" component={Tareas} />
       <Route exact path="/publicaciones/:key" component={Publicaciones} />
+      <Route exact path="/tareas/guardar" component={TareasGuardar} />
     </div>
   </BrowserRouter>
 );
